e6b5e49b39436e36f9d75079097895327eba1e84
Vincent Philaeux Lamotte <philaeux@the-cluster.org>
2018-01-30 01:56:11 +0100

    Refresh docs

--- backend/docs/api_project.js
+++ backend/docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-01-22T04:48:23.619Z",
+    "time": "2018-01-30T00:56:02.468Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
